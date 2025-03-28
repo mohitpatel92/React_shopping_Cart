87c9843e3b37fe56c1c49276cfdcc0ec22e0b39f
mohitpatel92 <dlinternationals@gmail.com>
2025-03-28 10:59:23 +0530

    someChanges

--- src/Components/navbar.jsx
+++ src/Components/navbar.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import {useSelector} from "react-redux"
-//import {getItemSelector} from "../Redux/Slice/Slice"
+import {getItemSelector} from "../Redux/Slice/Slice"
 
 const Nav = () => {
-  //const items = useSelector(getItemSelector)
-  const items = useSelector(state => state)
-  const total = items.cart.reduce((a,b) => a+b.price,0)
+  const items = useSelector(getItemSelector)
+  //const items = useSelector(state => state)
+  const total = items.reduce((a,b) => a+b.price,0)
   //console.log("item...",items);
   
   return (
@@ -68,7 +68,7 @@ const Nav = () => {
                   d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
                 />
               </svg>
-              <span className="badge badge-sm indicator-item">{items.cart.length}</span>
+              <span className="badge badge-sm indicator-item">{items.length}</span>
             </div>
           </div>
           <div
@@ -76,7 +76,7 @@ const Nav = () => {
             className="card card-compact bg-neutral text-neutral-content  dropdown-content bg-base-100 z-[1] mt-3 w-52 shadow"
           >
             <div className="card-body ">
-              <span className="text-lg font-bold">{items.cart.length} Items</span>
+              <span className="text-lg font-bold">{items.length} Items</span>
               <span className="text-info">Subtotal: ${total.toFixed(2)}</span>
               <div className="card-actions">
                 <button className="btn btn-primary btn-block">View cart</button>
--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,13 +1,4 @@
-// import { configureStore } from "@reduxjs/toolkit";
-// import cartReducer from "./Slice/Slice"
 
-// export const store = configureStore({
-//     reducer :{
-//         cart : cartReducer
-//     }
-// })
-
-// export default store
 
 import { configureStore } from "@reduxjs/toolkit";
 import cartreducer from "./Slice/Slice"
